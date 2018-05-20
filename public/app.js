36b84a858c111a9ead15f54f22c2146b1ca5d625
Wesley Hanson <wrh1600@gmail.com>
2018-05-20 14:27:40 -0700

    styling and display building

--- public/app.js
+++ public/app.js
@@ -2,9 +2,24 @@ function displayNews(news){
     $(".view").empty()
 
     news.forEach(function(info){
-        $(".view").append("<h1>" + info.title + "</h1>" +
-        "<a href=" + info.link + ">Read more</a>" + "<hr>");
-    })
+        $(".view").append("<div class='block'>" + 
+                            "<div class='columns'>" + 
+                                "<div class='column is-12'>" + 
+                                    "<h1>" + info.title + "</h1>" +
+                                "<div class='columns'>" + 
+                                    "<div class='column is-3'>" + "</div>" + 
+                                    "<div class='column is-3'>" +
+                                        "<a class='comments' href='#'" + ">Comments <i class='fa fa-comment'></i></a>"+
+                                    "</div>" + 
+                                    "<div class='column is-3'>" +
+                                        "<a class='link' href=" + info.link + ">Full Article <i class='fa fa-file-text'></i></a>"+ 
+                                    "</div>" + 
+                                    "<div class='column is-3'>" + "</div>" +   
+                                "</div>" +    
+                                "</div>" +                            
+                            "</div>" +
+                         "</div>" + "<hr>")  
+    });
 }
 
 $(document).ready(function(){
@@ -18,4 +33,4 @@ setTimeout(function(){
     $.getJSON("/all", function(stories){
         displayNews(stories);
     })
-},1500)    
+},1000)    
